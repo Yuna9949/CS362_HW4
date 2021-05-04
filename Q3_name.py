764ee45bcd4577bd6cc311d52caed01642998413
Yuna Oh <ustina0409@gmail.com>
2021-05-03 23:30:28 -0700

    print name

---
+++ Q3_name.py
@@ -0,0 +1,8 @@
+class Q3_name:
+    def name_print():
+        firstname = input('\nInput firstname : ')
+        lastname = input('Input  lastname : ')
+        fullname = firstname + ' '+ lastname
+        return fullname
+    
+    print(name_print())
